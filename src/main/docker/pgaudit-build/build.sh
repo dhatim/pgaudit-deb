cb84baf252792ffc5e5adcb6d6f1d9ef3a334160
Olivier Chédru <ochedru@dhatim.com>
2017-03-01 18:29:40 +0100

    Checkout stable branch of pgaudit

--- src/main/docker/pgaudit-build/build.sh
+++ src/main/docker/pgaudit-build/build.sh
@@ -3,6 +3,7 @@
 cd /build/postgres/contrib
 git clone https://github.com/pgaudit/pgaudit.git
 cd pgaudit
+git checkout REL9_6_STABLE
 make -s check
 cp /build/postgres/contrib/pgaudit/pgaudit.so /target
 echo 'build complete'
