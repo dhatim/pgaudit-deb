5f76be09692a768b6a9e00106c14eb08e52e2b0d
Olivier Chédru <ochedru@dhatim.com>
2017-03-01 21:33:10 +0100

    Publish on bintray after upload

--- .travis/script.sh
+++ .travis/script.sh
@@ -9,10 +9,12 @@ then
     mvn install
     
     # deploy to bintray
-    DEB_FILE_NAME="$(basename $TRAVIS_BUILD_DIR/target/*.deb)"
-    DEB_FILE_PATH="$TRAVIS_BUILD_DIR/target/$DEB_FILE_NAME"
-    echo "Deploying $DEB_FILE_NAME to bintray..."
-    curl -T "$DEB_FILE_PATH" -u${BINTRAY_NAME}:${BINTRAY_KEY} "https://api.bintray.com/content/dhatim/deb/pgaudit/${TRAVIS_TAG}/$DEB_FILE_NAME;deb_distribution=jessie;deb_component=main;deb_architecture=amd64"
+    DEB_FILE_NAME="$(basename ${TRAVIS_BUILD_DIR}/target/*.deb)"
+    DEB_FILE_PATH="${TRAVIS_BUILD_DIR}/target/${DEB_FILE_NAME}"
+    echo "Uploading ${DEB_FILE_NAME} to bintray..."
+    curl -T "${DEB_FILE_PATH}" -u${BINTRAY_NAME}:${BINTRAY_KEY} "https://api.bintray.com/content/dhatim/deb/pgaudit/${TRAVIS_TAG}/${DEB_FILE_NAME};deb_distribution=jessie;deb_component=main;deb_architecture=amd64"
+    echo "Publishing version ${TRAVIS_TAG}..."
+    curl -X POST -u${BINTRAY_NAME}:${BINTRAY_KEY} "https://api.bintray.com/content/dhatim/deb/pgaudit/${TRAVIS_TAG}/publish
 else
     # this is a regular build
     mvn install
