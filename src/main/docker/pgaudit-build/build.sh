552c8f939ca3884c96842cd9fdc1cc6837a4e1ad
Olivier Chédru <ochedru@dhatim.com>
2017-03-01 23:32:31 +0100

    Disable default jdeb exec

--- src/main/docker/pgaudit-build/build.sh
+++ src/main/docker/pgaudit-build/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -ex
 
 cd /build/postgres/contrib
-git clone https://github.com/pgaudit/pgaudit.git
+su builder -c "git clone https://github.com/pgaudit/pgaudit.git"
 cd pgaudit
-git checkout REL9_6_STABLE
-make -s check
+su builder -c "git checkout REL9_6_STABLE"
+su builder -c "make -s check"
 cp /build/postgres/contrib/pgaudit/pgaudit.so /target
 echo 'build complete'
