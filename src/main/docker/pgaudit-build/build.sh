09d341f6644004d27182a61b5cdff7ec8ac5e5df
Olivier Chédru <ochedru@dhatim.com>
2017-10-17 21:26:59 +0200

    Branch for pgaudit 1.0.6 (PostgreSQL 9.5)

--- src/main/docker/pgaudit-build/build.sh
+++ src/main/docker/pgaudit-build/build.sh
@@ -3,9 +3,9 @@
 cd /build/postgres/contrib
 su builder -c "git clone https://github.com/pgaudit/pgaudit.git"
 cd pgaudit
-su builder -c "git checkout REL9_6_STABLE"
+su builder -c "git checkout REL9_5_STABLE"
 su builder -c "make -s check"
 cp /build/postgres/contrib/pgaudit/pgaudit.so /target
 cp /build/postgres/contrib/pgaudit/pgaudit.control /target
-cp /build/postgres/contrib/pgaudit/pgaudit--1.0.sql /target
+cp /build/postgres/contrib/pgaudit/pgaudit--1.0.6.sql /target
 echo 'build complete'
